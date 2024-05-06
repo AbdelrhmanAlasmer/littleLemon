2fabb5ce48c71830b1749e453f6ef70ab42d5890
unknown <abdelrahman.alasmer123@gmail.com>
2024-05-06 20:51:07 +0300

    finall commit

--- littleLemon/urls.py
+++ littleLemon/urls.py
@@ -1,8 +1,8 @@
 """
-URL configuration for littlelemon project.
+URL configuration for LittleLemon project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -19,5 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('restraun.urls')),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.authtoken')),
+    path('restaurant/', include('restaurant.urls')),
 ]
--- restraun/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class RestraunConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "restraun"
